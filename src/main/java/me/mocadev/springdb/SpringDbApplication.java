a63a16e6de8ae153590f75a824c9bf4f21be7924
tony <chcjswo@gmail.com>
2022-06-26 00:15:46 +0900

    테스트 코드 구현

--- src/main/java/me/mocadev/springdb/SpringDbApplication.java
+++ src/main/java/me/mocadev/springdb/SpringDbApplication.java
@@ -1,13 +1,25 @@
 package me.mocadev.springdb;
 
+import me.mocadev.springdb.config.MemoryConfig;
+import me.mocadev.springdb.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "me.mocadev.springdb.web")
 public class SpringDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDbApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
 	}
 
 }
